7c38669e649668663069ab0b020c35dbafdeb6ba
Joseph Dvorak <dvorakjt@gmail.com>
2020-03-28 19:00:07 -0400

    added on click animations to buttons

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function renderTimeSlots() {
 
 updateDay();
 renderTimeSlots();
+
+$("button").on("click", function () {
+    var button = this;
+    $(button).empty();
+    var icon = $("<i>");
+    icon.addClass("far fa-save fa-spin");
+    $(button).append(icon);
+    var timer = setTimeout(function () {
+        $(button).empty();
+        var icon2 = $("<i>");
+        icon2.addClass("fas fa-check-square");
+        icon2.css("background-color", "white");
+        icon2.css("color", "limegreen");
+        icon2.css("padding-left", "1px");
+        icon2.css("padding-right", "1px");
+        $(button).append(icon2);
+    }, 2000);
+})
